f6626fed67292c57de5e5cc56bed9d4ba8b80aaa
Jeremy Buckley <jerry@connectworks.com>
2024-10-21 18:26:35 +1300

    Documentation and extensions of CalendarSupport.

--- jui-ui/src/main/java/com/effacy/jui/ui/client/control/CalendarSupport.java
+++ jui-ui/src/main/java/com/effacy/jui/ui/client/control/CalendarSupport.java
@@ -22,10 +22,51 @@ public class CalendarSupport {
         NONE, NUMERIC;
     }
 
+    /**
+     * For the given local and the given name obtain the (long-form) name of the
+     * month.
+     * 
+     * @param locale
+     *               the local to use (i.e. "en-gb").
+     * @param month
+     *               the month (indexed from 1).
+     * @return the name of the month.
+     */
     public static String nameOfMonth(String locale, int month) {
         return _formatDate(locale, 2000, month, 1, null, null, "long", null);
     }
 
+    /**
+     * See
+     * {@link #formatDate(String, int, int, int, FORMAT_WEEKDAY, FORMAT_YEAR, FORMAT_MONTH, FORMAT_DAY)}
+     * but where the year, month and day come from a {@link CalendarDate}.
+     */
+    public static String formatDate(String locale, CalendarDate date, FORMAT_WEEKDAY fWeekday, FORMAT_YEAR fyear, FORMAT_MONTH fMonth, FORMAT_DAY fDay) {
+        return formatDate(locale, date.year(), date.month(), date.day(), fWeekday, fyear, fMonth, fDay);
+    }
+
+    /**
+     * Given a locale and date (as a year, month and day) format the date subject to
+     * the formatting directives.
+     * 
+     * @param locale
+     *                 the local to use (i.e. "en-gb").
+     * @param year
+     *                 the year (i.e. 2023).
+     * @param month
+     *                 the month (indexed from 1).
+     * @param day
+     *                 the day (indexed from 1).
+     * @param fWeekday
+     *                 formatting for the day of the week (i.e. Sunday).
+     * @param fyear
+     *                 formatting for th year.
+     * @param fMonth
+     *                 formatting for the month.
+     * @param fDay
+     *                 formatting for the day.
+     * @return the formatted date.
+     */
     public static String formatDate(String locale, int year, int month, int day, FORMAT_WEEKDAY fWeekday, FORMAT_YEAR fyear, FORMAT_MONTH fMonth, FORMAT_DAY fDay) {
         return _formatDate(locale, year, month, day,
             ((fWeekday == null) || fWeekday == FORMAT_WEEKDAY.NONE) ? null : fWeekday.name().toLowerCase(),
@@ -35,7 +76,12 @@ public class CalendarSupport {
         );
     }
 
-    public static native String _formatDate(String locale, int year, int month, int day, String fWeekday, String fYear, String fMonth, String fDay) /*-{
+    /**
+     * Used by
+     * {@link #formatDate(String, int, int, int, FORMAT_WEEKDAY, FORMAT_YEAR, FORMAT_MONTH, FORMAT_DAY)}
+     * to pass through to the JS Date class.
+     */
+    private static native String _formatDate(String locale, int year, int month, int day, String fWeekday, String fYear, String fMonth, String fDay) /*-{
         if (locale == null)
             locale = 'en-us';
         var opts = {};
@@ -52,6 +98,20 @@ public class CalendarSupport {
         return new Date(year,month - 1,day).toLocaleDateString(locale, opts);
     }-*/;
 
+    /**
+     * Given a year, month and day normalise to a real date.
+     * <p>
+     * This accommodates for year, month and day data this is out-of-bound (i.e 30
+     * Feb, etc).
+     * 
+     * @param year
+     *              the year (i.e. 2023).
+     * @param month
+     *              the month (indexed from 1).
+     * @param day
+     *              the day (indexed from 1).
+     * @return a normalised calendar date instance.
+     */
     public static native CalendarDate normalise(int year, int month, int day) /*-{
         var date = new Date(year,month - 1,day);
         return new $wnd.com.effacy.jui.ui.client.control.CalendarDate(date.getFullYear(), date.getMonth() + 1, date.getDate());
@@ -71,6 +131,17 @@ public class CalendarSupport {
         return new Date(year,month,0).getDate();
     }-*/;
 
+    /**
+     * Obtains the day in the week of the given day (for a given month and year).
+     * 
+     * @param year
+     *              the year (i.e. 2023).
+     * @param month
+     *              the month (indexed from 1).
+     * @param day
+     *              the day (indexed from 1).
+     * @return the day of the week indexed from 1 and starting with Sunday.
+     */
     public static native int dayInWeek(int year, int month, int day) /*-{
         return new Date(year,month - 1,day).getDay() + 1;
     }-*/;

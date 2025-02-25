cd41bb13533f0b7b3cfd51373fdd7aadf124080b
Jeremy Buckley <jerry@connectworks.com>
2025-02-25 17:55:31 +1300

    Improve documentation and clean unused imports.

--- jui-maven-plugin/src/main/java/com/effacy/jui/maven/CompileMojo.java
+++ jui-maven-plugin/src/main/java/com/effacy/jui/maven/CompileMojo.java
@@ -3,7 +3,6 @@ package com.effacy.jui.maven;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
